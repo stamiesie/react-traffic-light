a25759f802ab6af95101e744b950dd02a6565022
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-11-03 10:35:37 -0700

    create active light hook

--- src/components/Light.js
+++ src/components/Light.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Light({ color }) {
+export default function Light({ color, active }) {
     return (
         <div>
-            <div className="light">{ color }</div>
+            <div className="light" style={{ backgroundColor: 
+                color === active ?
+            color: 'grey' }}></div>
         </div>
     )
 }
--- src/containers/LightContainer.jsx
+++ src/containers/LightContainer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Light from '../components/Light'
+import React, { useState } from 'react';
+import Light from '../components/Light';
 
 export default function LightContainer() {
     const colors = ['red', 'yellow', 'green'];
+
+    const [active, setActive] = useState('red');
     return (
         <div>
             {colors.map((color) => {
-                return <Light color = {color}/>
+                return <Light 
+                color={color}
+                active={active}/>
             })}
         </div>
     )
